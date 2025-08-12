36e5bdfbfb632d96c3a13a05fbc511f5726ad87b
Evenilson Liandro <evenilsonlp@gmail.com>
2025-08-12 11:20:00 -0300

    add: add initial configuration

---
+++ server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  }
+})
